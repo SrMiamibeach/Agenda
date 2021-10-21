1a3187ecd25093693e7defb96a21e3856809b984
Aaron <agomezdo20dw@ikzubirimanteo.com>
2021-10-21 13:14:40 +0200

    Personalizacion de cada agenda

--- index2.php
+++ index2.php
@@ -4,9 +4,9 @@
         <title>Login</title>
     </head>
     <body>
-        <form method="POST" action="agenda1.php">
+        <form method="POST" action="agenda2.php">
             <label>Introduce tu nombre</label>
-            <input type="text" name="userindex"/>
+            <input type="text" name="username"/>
             <input type="submit"/>
         </form>
     </body>
